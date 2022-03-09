59101f3ca88788ac96432e51cea3be733649ad20
jocosiol <jocosiol@gmail.com>
2022-03-09 14:53:25 +0200

    commit

--- create_new_folder_test.py
+++ create_new_folder_test.py
@@ -22,26 +22,26 @@ class Upword(unittest.TestCase):
         self.driver.find_element(by=By.ID, value="login-form-btn").click()
         time.sleep(10)
 
-    # def test_create_folder_from_button(self):
-    #     self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div/main/div/div[1]/div[2]/div/div[1]/div/div[2]/div/div/div/div[1]/div[2]/div").click()
-    #     self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[3]/div/div/form/div[1]/div/div/div[1]/div/input").send_keys("AA - Test Folder from rigth click")
-    #     self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[3]/div/div/form/div[2]/button[2]").click()
-    #     time.sleep(2)
-    #     assert self.driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'AA - Test Folder from rigth click')]")
-    #     self.actionChains.context_click(self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[1]/div/main/div/div[1]/div[2]/div/div[1]/div/div[2]/div/div/div/div[1]/div[2]/div[2]")).perform()
-    #     self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[1]/div/main/div/div[1]/div[2]/div/ul[1]/div/div/div/div[3]").click()
-    #     time.sleep(3)
+    def test_create_folder_from_button(self):
+        self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div/main/div/div[1]/div[2]/div/div[1]/div/div[2]/div/div/div/div[1]/div[2]/div").click()
+        self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[3]/div/div/form/div[1]/div/div/div[1]/div/input").send_keys("AA - Test Folder from rigth click")
+        self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[3]/div/div/form/div[2]/button[2]").click()
+        time.sleep(2)
+        assert self.driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'AA - Test Folder from rigth click')]")
+        self.actionChains.context_click(self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[1]/div/main/div/div[1]/div[2]/div/div[1]/div/div[2]/div/div/div/div[1]/div[2]/div[2]")).perform()
+        self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[1]/div/main/div/div[1]/div[2]/div/ul[1]/div/div/div/div[3]").click()
+        time.sleep(3)
     
-    # def test_create_folder_by_rigth_click(self):
-    #     self.actionChains.context_click(self.actionChains.move_by_offset(600,150).perform()).perform()
-    #     self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[1]/div/main/div/div[1]/div[2]/div/ul[2]/div").click()
-    #     self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[3]/div/div/form/div[1]/div/div/div[1]/div/input").send_keys("BB - Test Folder from Button")
-    #     self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[3]/div/div/form/div[2]/button[2]").click()
-    #     time.sleep(2)
-    #     assert self.driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'BB - Test Folder from Button')]")
-    #     self.actionChains.context_click(self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[1]/div/main/div/div[1]/div[2]/div/div[1]/div/div[2]/div/div/div/div[1]/div[2]/div[2]")).perform()
-    #     self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[1]/div/main/div/div[1]/div[2]/div/ul[1]/div/div/div/div[3]").click()
-    #     time.sleep(3)
+    def test_create_folder_by_rigth_click(self):
+        self.actionChains.context_click(self.actionChains.move_by_offset(600,150).perform()).perform()
+        self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[1]/div/main/div/div[1]/div[2]/div/ul[2]/div").click()
+        self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[3]/div/div/form/div[1]/div/div/div[1]/div/input").send_keys("BB - Test Folder from Button")
+        self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[3]/div/div/form/div[2]/button[2]").click()
+        time.sleep(2)
+        assert self.driver.find_elements(by=By.XPATH, value="//*[contains(text(), 'BB - Test Folder from Button')]")
+        self.actionChains.context_click(self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[1]/div/main/div/div[1]/div[2]/div/div[1]/div/div[2]/div/div/div/div[1]/div[2]/div[2]")).perform()
+        self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[1]/div/main/div/div[1]/div[2]/div/ul[1]/div/div/div/div[3]").click()
+        time.sleep(3)
 
     def test_create_subfolder_by_button(self):
         self.driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div/main/div/div[1]/div[2]/div/div[1]/div/div[2]/div/div/div/div[1]/div[2]/div").click()
